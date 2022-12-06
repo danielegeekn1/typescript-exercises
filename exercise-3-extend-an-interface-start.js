10518cf379dd8aa51ada7ba579234365e86f8944
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-12-06 13:57:09 +0100

    extend an interface ex checked and done

---
+++ exercise-3-extend-an-interface-start.js
@@ -0,0 +1,21 @@
+"use strict";
+/**
+ * START: Follow the instructions below.
+ */
+exports.__esModule = true;
+var countryA = {
+    name: "India",
+    code: "IN",
+    population: 1352642280
+};
+var countryB = {
+    name: "Italy",
+    code: "IT"
+};
+var countryC = {
+    name: "Spain",
+    code: "ES",
+    population: 47450795
+};
+// ----
+console.log(countryA, countryB, countryC);
