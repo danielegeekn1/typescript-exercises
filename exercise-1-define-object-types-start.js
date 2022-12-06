7db491fec1385dd965938f9d3fdd269e1942bc12
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-12-06 13:45:32 +0100

    add define object ex done

---
+++ exercise-1-define-object-types-start.js
@@ -0,0 +1,23 @@
+"use strict";
+/**
+ * START: Follow the instructions below.
+ */
+exports.__esModule = true;
+// Add a type annotation to this variable that describes the object it contains.
+var countryPopulation = {
+    code: "NZ",
+    population: 5135300
+};
+console.log(countryPopulation);
+var countryData = {
+    name: "India",
+    code: "IN",
+    population: 1352642280
+};
+console.log(countryData);
+var currencyData = {
+    name: "Euro",
+    code: "EUR",
+    symbol: "€"
+};
+console.log(currencyData);
