cb740fbcdf58b39e4ae83463b0e097cef9c4b081
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-12-07 18:53:56 +0100

    add ex construct types with record checked and done

---
+++ exercise-2-construct-types-with-record-start.js
@@ -0,0 +1,30 @@
+"use strict";
+/**
+ * START: Follow the instructions below.
+ */
+exports.__esModule = true;
+// Fix this use of the `Record` utility type so it constructs a valid type.
+var currency = {
+    name: "United States dollar",
+    code: "USD",
+    symbol: "$"
+};
+console.log(currency);
+var currencies = {
+    france: {
+        name: "Euro",
+        code: "EUR",
+        symbol: "€"
+    },
+    china: {
+        name: "Renminbi",
+        code: "CNY",
+        symbol: "¥"
+    },
+    colombia: {
+        name: "Colombian peso",
+        code: "COP",
+        symbol: "$"
+    }
+};
+console.log(currencies);
