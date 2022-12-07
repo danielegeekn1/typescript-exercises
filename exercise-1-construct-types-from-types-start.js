7be1365a8ffd711cd79eb463cfb546393fb49355
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-12-07 18:49:04 +0100

    add construct-types-from types ex checked and done

---
+++ exercise-1-construct-types-from-types-start.js
@@ -0,0 +1,39 @@
+"use strict";
+/**
+ * START: Follow the instructions below.
+ */
+exports.__esModule = true;
+var countryA = {
+    code: "CN",
+    population: 1412600000
+};
+console.log(countryA);
+var countryB = {
+    name: "Greece",
+    code: "GR",
+    languages: ["Greek"],
+    currency: "Euro",
+    population: 10678632
+};
+console.log(countryB);
+var countryC = {
+    name: "Italy",
+    code: "IT",
+    languages: ["Italian"],
+    population: 60317116
+};
+//countryC.population = 60_317_117;
+console.log(countryC);
+var countryD = {
+    name: "New Zealand",
+    code: "NZ",
+    population: 5135300
+};
+console.log(countryD);
+var countryE = {
+    name: "Thailand",
+    code: "TH",
+    languages: ["Thai", "Isan", "Kam Mueang", "Pak Tai", "Malay"],
+    currency: "Baht"
+};
+console.log(countryE);
